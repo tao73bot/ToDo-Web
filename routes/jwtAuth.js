cd65f0175b3bc1dd180c1c0b498d4f6ffe35d037
Md.Taohidul Islam <67187868+tao73bot@users.noreply.github.com>
2023-07-23 01:25:44 +0600

    login system added

--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -41,4 +41,32 @@ router.post("/register", async (req, res) =>{
     }
 });
 
+// login route
+
+router.post("/login",async (req,res) => {
+    try {
+        // req.body
+        const {email, password} = req.body;
+        // check if user doesn't exist
+        const user = await pool.query("SELECT * FROM users WHERE user_email = $1",[email]);
+
+        if(user.rows.length === 0 ) {
+            return res.status(401).json("Password or Email is incorrect");
+        }
+
+        // check if incoming password is the same then database password
+
+        const validPassword = await bcrypt.compare(password, user.rows[0].user_password);
+        if(!validPassword){
+            return res.status(401).json("Password or Email is incorrect.");
+        }
+        // give jwt token
+
+        const token = jwtGenerator(user.rows[0].user_id);
+        res.json({token});
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
 module.exports = router;
